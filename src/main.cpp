8e611b792a25d1573acf2939b7664f3c75b2025b
Lino Pegoretti <lino.pegoretti@ellevo.com>
2020-06-05 19:36:08 -0300

    Fixando IP e implantando OTA

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,43 @@
 #include <ESP8266WiFi.h>       // Servidor Wifi
 #include <ESP8266WebServer.h>  // WebServer
-#include <ESP8266mDNS.h>       //Servidor dns
 #include <WiFiClient.h>        // Cliente Wifi
 #include <ArduinoJson.h>       //Serializa e desserializa Json
 #include <OneWire.h>           // Sensor um fio
 #include <DallasTemperature.h> //Sensor Dallas usado o DS18B20
+#include <ArduinoOTA.h>
+
+IPAddress staticIP(192, 168, 2, 200); //ESP static ip
+IPAddress gateway(192, 168, 2, 1);    //IP Address of your WiFi Router (Gateway)
+IPAddress subnet(255, 255, 255, 0);   //Subnet mask
+IPAddress dns(181, 213, 132, 2);      //DNS
 
 ESP8266WebServer server(80);
 const char *wifiName = "LINO-WIFI";
 const char *wifiPass = "22051974";
 
-const int oneWireBus = 5;            // GPIO onde o sensor está conectado
+const int oneWireBus = 5;            // D1 GPIO onde o sensor está conectado
 OneWire oneWire(oneWireBus);         // Objeto oneWire para comunicação com o sensor
 DallasTemperature sensors(&oneWire); // Passando a referencia do sensor Dallas para o objeto onewire
-//Conexões LED
-const int LEDVermelho = 4; //D2  GPIO4
-const int LEDVerde = 0;    //D3  GPIO0
-const int LEDAzul = 2;     //D4  GPIO2
+//Conexões rele
+const int LED = 4;  //D2  GPIO4
+const int rele = 0; //D3  GPIO0
+
+float temperaturaEsperada = 18;
 
 void getTemperatura()
 {
   sensors.requestTemperatures();
   float temperaturaAtual = sensors.getTempCByIndex(0);
+  Serial.print("Get: ");
   Serial.print(temperaturaAtual);
-  Serial.println("ºC");
+  Serial.println(" ºC");
 
   const size_t capacity = JSON_ARRAY_SIZE(3) + 5 * JSON_OBJECT_SIZE(2);
   DynamicJsonDocument doc(capacity);
 
   JsonObject Atual = doc.createNestedObject("Atual");
   Atual["Temperatura"] = temperaturaAtual;
-  Atual["Setup"] = 18;
+  Atual["Setup"] = temperaturaEsperada;
 
   JsonArray Rampas = doc.createNestedArray("Rampas");
 
@@ -54,13 +61,18 @@ void getTemperatura()
 
 void setTemperatura()
 {
-  String retorno = "Nada";
-  if (server.hasArg("plain") == true)
-  {
-    retorno = server.arg("plain");
-  }
+  const size_t capacity = JSON_ARRAY_SIZE(3) + 5 * JSON_OBJECT_SIZE(2);
+  DynamicJsonDocument doc(capacity);
+  DeserializationError error = deserializeJson(doc, server.arg("plain"));
+  Serial.println(server.arg("plain"));
   server.sendHeader("Access-Control-Allow-Origin", "*");
-  server.send(200, "application/json", "{\"Recebido\":" + retorno + "}");
+  if (error)
+  {
+    server.send(400, "application/json", "{\"msg\":\"Erro no request\"");
+    return;
+  }
+  temperaturaEsperada = doc["Setup"];
+  server.send(200, "application/json", server.arg("plain"));
 }
 void setRampa()
 {
@@ -91,12 +103,39 @@ void setup(void)
   Serial.println();
   Serial.println("Iniciando... ");
   Serial.printf("Conectando a rede wifi: %s ", wifiName);
+  WiFi.config(staticIP, subnet, gateway, dns);
   WiFi.begin(wifiName, wifiPass);
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(250);
     Serial.print('.');
-  }
+  }  
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready OTA ESP8266");
   Serial.println('\n');
   Serial.print("Conectado a: ");
   Serial.print(WiFi.SSID());
@@ -104,29 +143,17 @@ void setup(void)
   Serial.print("Endereço IP: ");
   Serial.print(WiFi.localIP());
   Serial.println('\n');
-  if (MDNS.begin("ESP8266"))
-  { // Start the mDNS responder for esp8266.local
-    Serial.println("mDNS responder started");
-  }
-  else
-  {
-    Serial.println("Error setting up MDNS responder!");
-  }
+  
   Serial.println('\n');
   Serial.print("Inicializando o Sensor na porta: ");
   Serial.print(oneWireBus);
   Serial.println('\n');
   sensors.begin();
-  Serial.print("Inicializando as portas do LED RGB: ");
-  Serial.print("Vermelho: ");
-  pinMode(LEDVermelho, OUTPUT);
-  Serial.println("OK ");
-  Serial.print("Verde: ");
-  pinMode(LEDVerde, OUTPUT);
-  Serial.println("OK ");
-  Serial.print("Azul: ");
-  pinMode(LEDAzul, OUTPUT);
-  Serial.println("OK ");
+  Serial.println("Inicializando o rele: ");
+  Serial.print("Rele: ");
+  pinMode(rele, OUTPUT);
+  pinMode(LED, OUTPUT);
+  Serial.println("Ok");
 
   // Chama a função 'handleRoot' quando buscar o endereço 'http://enderecoControlador'
   server.on("/", handleRoot);
@@ -135,7 +162,7 @@ void setup(void)
   server.on("/temperatura", HTTP_GET, getTemperatura);
 
   //  Chama a função 'setTemperatura' quando buscar o endereço 'http://enderecoControlador/temperatura' e usar método POST
-  server.on("/temperatura", HTTP_POST, setTemperatura);
+  server.on("/temperaturaPost", HTTP_POST, setTemperatura);
 
   //  Chama a função 'setRampa' quando buscar o endereço 'http://enderecoControlador/rampa' e usar método POST
   server.on("/rampa", HTTP_POST, setRampa);
@@ -149,21 +176,25 @@ void setup(void)
 
 void loop(void)
 {
-  digitalWrite(LEDVermelho, LOW);
-  digitalWrite(LEDVerde, LOW);
-  digitalWrite(LEDAzul, LOW);
-  //digitalWrite(LEDVermelho, HIGH);
-  // digitalWrite(LEDVerde, LOW);
-  // digitalWrite(LEDAzul, LOW);
-  // delay(1000);
-  // digitalWrite(LEDVermelho, LOW);
-  // digitalWrite(LEDVerde, HIGH);
-  // digitalWrite(LEDAzul, LOW);
-  // delay(1000);
-  // digitalWrite(LEDVermelho, LOW);
-  // digitalWrite(LEDVerde, LOW);
-  // digitalWrite(LEDAzul, HIGH);
-  // delay(1000);
-  // Aguarda requisições do cliente
+  ArduinoOTA.handle();
   server.handleClient();
+  sensors.requestTemperatures();
+  float temperaturaAtual = (int)sensors.getTempCByIndex(0);
+
+  if (temperaturaAtual >= temperaturaEsperada + 1)
+  {
+    // Serial.print("Liga: ");
+    // Serial.println(temperaturaAtual);
+    digitalWrite(rele, LOW);
+    digitalWrite(LED, HIGH);
+    return;
+  }
+  if (temperaturaAtual <= temperaturaEsperada - 1)
+  {
+    // Serial.print("Liga: ");
+    // Serial.println(temperaturaAtual);
+    digitalWrite(rele, HIGH);
+    digitalWrite(LED, LOW);
+    return;
+  }
 }
